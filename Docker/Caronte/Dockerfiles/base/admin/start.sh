7f0c0e94ea2cb4b6a377572e1abf768175c0c9fd
Amaeth <sniukis98@gmail.com>
2025-11-05 12:30:18 +0100

    usuario script

--- Docker/Caronte/Dockerfiles/base/admin/start.sh
+++ Docker/Caronte/Dockerfiles/base/admin/start.sh
@@ -1,5 +1,18 @@
 #!bin/bash
 
-#Encargada de dejar este contenedor vivo en BGround
-tail -f /dev/null
-# Script que se encarga de configurar la imgen/contenedor
+newUser(){
+    useradd -rm -d /home/${USUARIO} -s /bin/bash ${USUARIO}
+    echo "${USUARIO}:1234" | chpasswd
+    echo "Bienvenida ${USUARIO} a tu empresa..." > /home/${USUARIO}/bienvenida.txt
+}
+
+main (){
+    newUser
+    #llamada a la funcion newUser
+    # Encargada de dejar este contenedor vivo en BGround
+    tail -f /dev/null
+    # Script que se encarga de configurar la imgen/contenedor
+}
+
+main 
+# llamada a la funcion "main"
