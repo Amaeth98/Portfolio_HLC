763f33aa323cf514c4d5eee1c65793270a1ccaeb
Amaeth <sniukis98@gmail.com>
2025-11-18 13:16:37 +0100

    ssh

--- Docker/Caronte/Dockerfiles/base/admin/SSH/mainssh.sh
+++ Docker/Caronte/Dockerfiles/base/admin/SSH/mainssh.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -e
 
 configurar_ssh() {
 
     #Deshabilitar login de root por ssh
-    sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+    sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
     #Cambiar el puerto ssh
     sed -i 's/#Port.*/Port '$PORT_SSH'/' /etc/ssh/sshd_config
