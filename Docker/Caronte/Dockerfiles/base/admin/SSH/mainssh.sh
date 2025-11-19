0c22addaaa6f1f375e2e622d2bbf99fbaabe2aa1
Amaeth <sniukis98@gmail.com>
2025-11-19 11:51:56 +0100

    cp en start.sh

--- Docker/Caronte/Dockerfiles/base/admin/SSH/mainssh.sh
+++ Docker/Caronte/Dockerfiles/base/admin/SSH/mainssh.sh
@@ -16,15 +16,18 @@ configurar_ssh() {
     #Reiniciar el servicio ssh
     service ssh restart
 
+    mkdir -p /home/${USUARIO}/.ssh
+    cp /root/admin/base/common/id_ed25519.pub /home/${USUARIO}/.ssh/authorized_keys
+
 }
 # /etc/init.d/ssh start &
 # exec /usr/sbin/sshd -D & (dejar el ssh en background; segundo plano)
 
 #adri ALL=(ALL:ALL) ALL
-config_sudoers() {
-    if [ -f /etc/sudoers ]
-    then
-        #comprobar que el ${USUARIO} No existe
-        echo "${USUARIO} ALL=(ALL:ALL) ALL" >> /etc/sudoers
-    fi
-}
+#config_sudoers() {
+#    if [ -f /etc/sudoers ]
+#    then
+#        #comprobar que el ${USUARIO} No existe
+#        echo "${USUARIO} ALL=(ALL:ALL) ALL" >> /etc/sudoers
+#    fi
+#}
